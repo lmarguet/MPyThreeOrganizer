910888aa3e5656e74ba987479058a7b757bdd337
lmarguet <lucasmarguet@gmail.com>
2018-05-07 12:11:01 +0200

    Fix repo

--- UdemyPythonMegaCourse/app.py
+++
@@ -1,5 +0,0 @@
-import folium
-
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a marker", icon=folium.Icon(color='green')))
-map.save("Map.html")
